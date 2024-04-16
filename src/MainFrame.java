81a831554414d311e14d738bc375168544fe0530
PerssonO <1984.ola.persson@gmail.com>
2024-04-16 09:58:31 +0200

    merge

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -7,17 +7,21 @@ public class MainFrame extends JFrame {
     private KartPanel kartPanel = new KartPanel();
     private CheckInPanel checkInPanel = new CheckInPanel();
 
+    private VmaPanel vmaPanel = new VmaPanel();
+
 
     public  MainFrame() {
         this.setTitle("KRIS");
         this.setSize(800,600);
         setupGUI();
+        VmaController.fetchAndDisplayVmaData(vmaPanel);
     }
 
     private void setupGUI() {
 
         tabs.addTab("Karta", kartPanel);
         tabs.addTab("Checka in/ut", checkInPanel);
+        tabs.addTab("Visa VMA-meddelande",vmaPanel);
         this.add(tabs, BorderLayout.CENTER);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
---
+++ src/VmaPanel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class VmaPanel extends JPanel {
+    private JTextArea textArea;
+
+    public VmaPanel() {
+        setPreferredSize(new Dimension(800, 600));
+        textArea = new JTextArea(35, 70);
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        this.setLayout(new BorderLayout());
+        this.add(scrollPane, BorderLayout.CENTER);
+    }
+    public void appendText(String text) {
+        textArea.append(text + "\n");
+    }
+}
